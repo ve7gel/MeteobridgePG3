c0a709c3814fc9b1666d520e1002a094fda047bd
ve7gel <o31254G!>
2021-10-28 16:23:13 -0700

    Update node modules code

--- nodes/PrecipNode.py
+++ nodes/PrecipNode.py
@@ -4,7 +4,6 @@ Polyglot v3 node server for Meteobridge
 Copyright (C) 2021 Gordon Larsen
 """
 import udi_interface
-import sys
 import uom
 
 LOGGER = udi_interface.LOGGER
@@ -35,21 +34,6 @@ class PrecipNode(udi_interface.Node):
         self.Parameters.load(params)
         self.units = self.Parameters['Units']
 
-    """
-    def poll(self, polltype):
-
-        if 'shortPoll' in polltype:
-            if self.Parameters['multiplier'] is not None:
-                mult = int(self.Parameters['multiplier'])
-            else:
-                mult = 1
-
-            self.count += 1
-
-            self.setDriver('GV0', self.count, True, True)
-            self.setDriver('GV1', (self.count * mult), True, True)
-    """
-
     def set_Driver(self, driver, value, **kwargs):
         if self.units == "us":
             value = (value * 1.8) + 32  # convert to F
@@ -59,7 +43,7 @@ class PrecipNode(udi_interface.Node):
     def create_drivers(self):
         self.rain_list['rate'] = 'I_MMHR' if self.units == 'metric' else 'I_INHR'
         self.rain_list['daily'] = 'I_MM' if self.units == 'metric' else 'I_INCHES'
-        self.rain_list['24hour'] = 'I_MM' if self.nits == 'metric' else 'I_INCHES'
+        self.rain_list['24hour'] = 'I_MM' if self.units == 'metric' else 'I_INCHES'
         self.rain_list['yesterday'] = 'I_MM' if self.units == 'metric' else 'I_INCHES'
         self.rain_list['monthly'] = 'I_MM' if self.units == 'metric' else 'I_INCHES'
         self.rain_list['yearly'] = 'I_MM' if self.units == 'metric' else 'I_INCHES'
